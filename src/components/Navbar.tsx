ab76108f4595239097c02b5b954d63f7d6c04611
A Holcsik <you@example.com>
2024-08-02 16:43:45 +0200

    misc

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,12 @@ import React from "react";
 
 const Navbar = (props) => {
   return (
-    <form onSubmit={}>
+    <div>
       <div className="flex justify-end p-4">
         <div className="mb-3 xl:w-96">
           <div className="relative mb-4 flex w-full flex-wrap items-stretch">
             <input
               type="search"
-              onChange={}
               className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
               placeholder="Search location"
               aria-label="Search"
@@ -25,7 +24,7 @@ const Navbar = (props) => {
           </div>
         </div>
       </div>
-    </form>
+    </div>
   );
 };
 
--- utils/weather.ts
+++ utils/weather.ts
@@ -1,6 +1,6 @@
 export async function getWeather() {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=peshawar&appid=${process.env.WEATHER_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=szeged&appid=${process.env.WEATHER_API_KEY}`
   );
   if (!response.ok) throw new Error("failed to fetch weather");
   const data = await response.json();

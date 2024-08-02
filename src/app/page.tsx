172efc1a8bb189ae721f74d476e32501c594e3ad
A Holcsik <you@example.com>
2024-08-02 14:13:02 +0200

    added tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "@/components/Navbar";
+import WeatherCard from "@/components/WeatherCard";
 
 export default function Home() {
-  return <div>The weather app</div>;
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <WeatherCard />
+      </div>
+    </div>
+  );
 }
